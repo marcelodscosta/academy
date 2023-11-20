9ee9ef9f5fad46a5a72e57aba21dcd0569a78014
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-11-19 21:35:05 -0300

    instalação 2 libs para utilização do svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -4,11 +4,12 @@ import {
 } from '@gluestack-ui/themed';
 
 import BackgroundImg from '../assets/background.png';
+import LogoSvg from '../assets/logo.svg';
 
 export function SignIn() {
   return (
     <VStack flex={1} bg='#121214'>
-
+      <LogoSvg />
       <Image
         source={BackgroundImg}
         alt='academy'
